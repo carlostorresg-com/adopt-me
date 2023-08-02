1fc201068a55992b4458ded2f621af300ef27e97
Carlos Torres <ctgovea@gmail.com>
2023-08-02 16:48:32 -0600

    add Pet component

---
+++ src/App.js
@@ -0,0 +1,38 @@
+const Pet = (props) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, props.name),
+    React.createElement('h2', {}, props.animal),
+    React.createElement('h2', {}, props.breed),
+  ])
+}
+
+const App = () => {
+  // a component should always return markup, which is the result of createElement
+  return React.createElement(
+    'div', // what kind of element this is
+    {}, // attributes
+    [
+      React.createElement('h1', {}, 'Adopt Me!'),
+      React.createElement(Pet, {
+        animal: 'Dog',
+        name : 'Luna',
+        breed: 'Havanese',
+      }), 
+      React.createElement(Pet, {
+        animal: 'Bird',
+        name : 'Pepper',
+        breed: 'Cockatiel',
+      }),
+      React.createElement(Pet, {
+        animal: 'Dog',
+        name : 'Tyrsi',
+        breed: 'Chihuahua',
+      }),
+    ]
+    // children
+  )
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
