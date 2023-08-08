a14211aa91d81def3cad1b22b7b113f943b523f5
Carlos Torres <ctgovea@gmail.com>
2023-08-08 17:03:58 -0600

    add typescript

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,19 +1,31 @@
-import { useEffect, useRef } from 'react';
+import React, {
+  useEffect,
+  useRef,
+  MutableRefObject,
+  ReactElement,
+} from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = ({ children }) => {
-  const elRef = useRef(null);
+const Modal = ({ children }: { children: ReactElement }) => {
+  const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
   if (!elRef.current) {
     elRef.current = document.createElement('div');
   }
 
   useEffect(() => {
     const modalRoot = document.getElementById('modal');
+    if (!modalRoot || !elRef.current) {
+      return;
+    }
     modalRoot.appendChild(elRef.current);
 
     // we return a function to have a way to close the modal
     // a function returned in useEffect will run when a component has been unmounted from the DOM
-    return () => modalRoot.removeChild(elRef.current);
+    return () => {
+      if (elRef.current) {
+        modalRoot.removeChild(elRef.current);
+      }
+    };
   }, []);
 
   return createPortal(<div>{children}</div>, elRef.current);
